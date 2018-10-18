4667daf2bf910baeb86ae8534fce7bc99ba0ca53
Mikaël Mayer <mikael.mayer@epfl.ch>
2018-10-18 11:53:40 +0200

    SNS: Edit lenses updatd. @c5ca7ee26588f1c88a1e59a973e47ff60705a549

---
+++ wysiwyg/src/index.elm
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meta name="website" url="https://mikaelmayer.github.io/wysiwyg/">
+    <meta name="source" url="src/index.elm">
+    <meta name="output" url="index.html">
+  </head>
+  <body>
+    
+  </body>
+</html>
